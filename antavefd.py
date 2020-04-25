50a1c6836d9fde5ba5c95e6007312141f5b5ffb1
Isaac Ancell <isaacancell@gmail.com>
2020-04-25 01:38:17 -0700

    Added simple functionality using multiple streams
    It functions as intended and successfully launches

--- antavefd.py
+++ antavefd.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
 
-#downloadFile = "https://antave.com/onewebmedia/Drive-By_Alpha_2.7.apk"
-#downloadFile = "https://download141.uploadhaven.com/1/application/zip/Oezi6X1nTpVwpzpOP0wtciNkBurMwMl0QbDRI28U.zip?key=An4NDatyQCvDh2HOVmW4Kg&expire=1587797054&filename=DOOM.Eternal.Deluxe.Edition.zip"
-downloadFile = "http://download1583.mediafire.com/a40ljfnhtagg/0mxww72t6dwl0i8/Server.zip"
-process = subprocess.Popen(["curl", "-s", "-L", "-I", downloadFile], stdout=subprocess.PIPE)
+#initializes the variables
+downloadFile = raw_input("Enter Download URL: ")
+extension = raw_input("Enter Download Extension (ex: .zip, .png, .exe,): ")
+
+#determines the size of the download
+process = subprocess.Popen(["curl", "-L", "-I", downloadFile], stdout=subprocess.PIPE)
 stdout = process.communicate()[0]
 print 'STDOUT:{}'.format(stdout) 
 
@@ -19,20 +21,31 @@ fileSize = int(character)
 
 print fileSize
 
+#initializes some variables
 startPacket = 0
 endPacket = 10000000
+repeat = " & "
+downloadCommand = "echo \"Starting Download\" & "
 repeatAmount = (fileSize / 10000000) + 1
 
+#repeats for all download streams
 for x in range(repeatAmount):
-	if x == repeatAmount:
+	if x == repeatAmount - 1:
 		endPacket = fileSize
+		repeat = " & wait"
 
 	if x == 1:
 		startPacket += 1
 
-	os.system("curl -r " + str(startPacket) + "-" + str(endPacket) + " -o Block" + str(x) + ".bin " + downloadFile)
+	downloadCommand += "curl -r " + str(startPacket) + "-" + str(endPacket) + " -o Block" + str(x) + ".bin " + downloadFile + repeat
 	startPacket += 10000000
 	endPacket += 10000000
 
+#executes download for all streams
+os.system(downloadCommand)
 
-#os.system("curl -s -L -I https://antave.com/onewebmedia/Drive-By_Alpha_2.7.apk | gawk -v IGNORECASE=1 '/^Content-Length/ { print $2 }'");
+#finishes up download and performs a checksum with existing files with same extension
+os.system("cat Block*.bin > output" + extension)
+os.system("echo \"Download Complete!\"")
+os.system("rm Block*.bin")
+os.system("cksum *" + extension)
